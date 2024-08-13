e59bbb4e88db56a0d47de5678b118e64a2cfa61c
Vince <112585869+vinn03@users.noreply.github.com>
2024-08-12 22:40:37 -0700

    partial completion of experience section

--- portfolio/src/components/experience/components/exp-component.tsx
+++ portfolio/src/components/experience/components/exp-component.tsx
@@ -4,20 +4,42 @@ interface ExpProps {
   title: string;
   company: string;
   duration: string;
+  technologies: string[];
   description: string;
 }
+
+interface SkillProps {
+  tech?: string;
+}
+
+const Skill: React.FC<SkillProps> = ({ tech }) => {
+  return (
+    <div
+      className="rounded-lg text-center p-2 pl-4 pr-4 text-xs md:text-sm"
+      style={{ backgroundColor: "rgba(0, 13, 50, 0.8)" }}
+    >
+      {tech}
+    </div>
+  );
+};
 
 const ExpComponent: React.FC<ExpProps> = ({
   title,
   company,
   duration,
+  technologies,
   description,
 }) => {
   return (
     <div className="bg-blue-400 bg-opacity-20 p-4 rounded-lg shadow-md relative">
-      <h1 className="text-xl font-bold font-mono text-blue-100 mb-2">
-        {title}
-      </h1>
+      <span className="flex flex-row justify-between mb-2">
+        <h1 className="text-xl font-bold font-mono text-blue-100 mb-2">
+          {title}
+        </h1>
+        <span className="grid grid-rows-1 grid-flow-col gap-4 text-blue-300 font-mono">
+          {technologies?.map((tech) => <Skill tech={tech} />)}
+        </span>
+      </span>
       <span className="text-sm font-mono mb-8 text-blue-300 flex justify-between">
         <h3 className="opacity-70 inline">{company}</h3>
         <h3 className="opacity-70 inline">{duration}</h3>
--- portfolio/src/components/experience/experience.tsx
+++ portfolio/src/components/experience/experience.tsx
@@ -2,13 +2,23 @@ import ExpComponent from "./components/exp-component";
 
 const Experience = () => {
   return (
-    <div className="bg-blue-400 bg-opacity-20 p-4 rounded-lg shadow-md relative">
+    <div className="bg-opacity-20 p-4 rounded-lg shadow-md relative">
+      <h2 className="text-2xl md:text-4xl pt-8 pb-6 md:pb-12 font-sans text-blue-100 text-center font-bold">
+        Experience
+      </h2>
       <ExpComponent
-        title="Software Developer"
-        company="Company Name"
-        duration="Jan 2021 - Present"
-        description="I am a software developer working on a variety of projects. 
-        I have experience with front-end and back-end technologies, as well as deployment and testing."
+        title="Software Engineering Intern"
+        company="Apple Inc."
+        duration="MAY 2024 - PRESENT"
+        technologies={[
+          "React",
+          "Redux",
+          "TypeScript",
+          "CSS",
+          "MapKit JS",
+          "deck.gl",
+        ]}
+        description="Interned with the Apple Maps team. Worked on the development of an internal data visualization application."
       />
     </div>
   );
