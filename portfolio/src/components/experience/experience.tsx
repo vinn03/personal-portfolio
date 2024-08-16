3e094b517b68d886e59d985bece575072615834c
Vince <112585869+vinn03@users.noreply.github.com>
2024-08-16 12:18:42 -0700

    swapped exp

--- portfolio/src/components/experience/experience.tsx
+++ portfolio/src/components/experience/experience.tsx
@@ -9,22 +9,22 @@ const Experience = () => {
       <div className="md:flex md:flex-row md:justify-between md:mr-36 md:ml-36">
         <span className="bg-opacity-20 p-4 w-1/2 rounded-lg shadow-md relative">
           <ExpComponent
-            title="Coding Instructor"
-            company="Roboplanet"
-            location="Richmond, BC"
-            duration="SEP 2022 - MAR 2023"
-            technologies={["Python", "Scratch"]}
-            description="Taught students aged 6-12 the basics of programming in Python and Scratch. Developed and delivered lesson plans, and provided guidance to students on their assignments and projects."
-          />
-        </span>
-        <span className="bg-opacity-20 p-4 w-1/2 rounded-lg shadow-md relative">
-          <ExpComponent
             title="Software Engineering Intern"
             company="Apple Inc."
             location="Cupertino, CA"
             duration="MAY 2024 - PRESENT"
             technologies={["React", "Redux", "TypeScript", "CSS"]}
             description="Interned with the Apple Maps team. Developed, and deployed, an internal application that allows users to visualize large-scale datasets."
+          />
+        </span>
+        <span className="bg-opacity-20 p-4 w-1/2 rounded-lg shadow-md relative">
+          <ExpComponent
+            title="Coding Instructor"
+            company="Roboplanet"
+            location="Richmond, BC"
+            duration="SEP 2022 - MAR 2023"
+            technologies={["Python", "Scratch"]}
+            description="Taught students aged 6-12 the basics of programming in Python and Scratch. Developed and delivered lesson plans, and provided guidance to students on their assignments and projects."
           />
         </span>
       </div>
