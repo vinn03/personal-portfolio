c5d2deb9bd631980f99e5f8e075780b649ad6d24
Vince <vincent_liu_5@sfu.ca>
2025-08-23 16:13:22 -0700

    updated portfolio at end of summer 2025

--- portfolio/src/components/bio/bio.tsx
+++ portfolio/src/components/bio/bio.tsx
@@ -1,26 +1,21 @@
 function Bio() {
   return (
     <p className="font-mono text-base text-blue-100">
-      Originally a Human Geography major, my interest in computing science and
-      software development sparked after taking my first programming course in
-      Summer 2022.
+      I'm currently studying both computing science and geographic information
+      science (GIS) at Simon Fraser University (SFU).
       <br></br> <br></br>
-      The process of problem-solving and creating programs proved to be
-      incredibly fulfilling, fuelling my desire to transfer into the School of
-      Computing Science and to further pursue such rewarding efforts.
+      Originally as a Human Geography major, I started pursuing programming
+      early-on, finding fulfillment in solving niche problems across different
+      modalities.
       <br></br> <br></br>
-      Since then, I've attended many hackathons and have worked towards a
-      transfer into the joint major between computing science and geographic
-      information science at SFU. <br></br> <br></br>
-      This goal was finally achieved in Summer 2023. <br></br> <br></br>
-      Currently, I am honing my skills in{" "}
-      <strong>full-stack development</strong>, with interest in{" "}
-      <strong>web GIS</strong>. I have also been refining my skills in
-      <strong> data analytics</strong> and <strong>data science</strong>.
+      With proficiency in GIS-related front-end development and data science, I
+      am eager to explore opportunities in the geospatial realm and am always
+      open to new challenges and learning experiences across other realms.
       <br></br> <br></br>
-      In my down time, I enjoy playing video games (especially role-playing
-      games), jamming on the guitar + bass + drums, and diving into topics such
-      as urbanism, anthropology, and history. <br></br> <br></br>
+      In my down time, I enjoy playing role-playing games (i.e. Final Fantasy)
+      and first-person shooters (i.e. Battlefield), jamming on the guitar, bass,
+      and drums, and deep diving into urbanism, anthropology, and history.{" "}
+      <br></br> <br></br>
     </p>
   );
 }
